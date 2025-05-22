60129ee690f4974771a31e3c52a2f9f83b7259d3
baenavly <baenavly@yonsei.ac.kr>
2025-05-22 09:39:08 +0900

    3코까지 완료

--- services/State.js
+++ services/State.js
@@ -1,5 +1,10 @@
 export async function getChampions() {
-  const response = await fetch("./data/champions.json");
+  const response = await fetch("data/champions.json");
   const data = await response.json();
-  return data;
+
+  // id 기반으로 image 경로 추가
+  return data.map((champion) => ({
+    ...champion,
+    image: `images/${champion.id}.jpg`, // id를 기반으로 경로 생성
+  }));
 }
